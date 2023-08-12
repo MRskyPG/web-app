69561b2b50127b0b2cc328674843f731a97df01b
MRskyPG <mrogalsky8113@gmail.com>
2023-08-12 12:25:51 +0600

    Add method for run HTTP Server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"github.com/MRskyPG/web-app"
+	"github.com/MRskyPG/web-app/pkg/handler"
+	"log"
+)
+
 func main() {
+	var handlers handler.Handler
+	var srv web.Server
+
+	if err := srv.Run("80", handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running http server: %s", err.Error())
+	}
 
 }
