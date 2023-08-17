fecb25793c0c02909fd0e50b9ff4f5bbc5f330fb
MRskyPG <mrogalsky8113@gmail.com>
2023-08-17 20:39:13 +0700

    Add handler functions for web.WorkingPosition

--- pkg/handler/staff.go
+++ pkg/handler/staff.go
@@ -1,25 +1,101 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/MRskyPG/web-app"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func (h *Handler) createPosition(c *gin.Context) {
+	var workingPosition web.WorkingPosition
 
+	if err := c.BindJSON(&workingPosition); err != nil {
+		fmt.Printf("An error occured: failed to bind JSON [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	h.service.InsertPosition(&workingPosition)
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"position_id": workingPosition.PositionID,
+	})
 }
 
 func (h *Handler) getAllPositions(c *gin.Context) {
-
+	positions := h.service.GetAllPositions()
+	c.JSON(http.StatusOK, positions)
 }
 
 func (h *Handler) getPositionById(c *gin.Context) {
+	position_id, err := strconv.Atoi(c.Param("position_id"))
 
+	if err != nil {
+		fmt.Printf("An error occured: failed to convert position_id to int [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	var workingPosition web.WorkingPosition
+	workingPosition, err = h.service.GetPosition(position_id)
+
+	if err != nil {
+		fmt.Printf("An error occured: [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, workingPosition)
 }
 
 func (h *Handler) updatePosition(c *gin.Context) {
+	position_id, err := strconv.Atoi(c.Param("position_id"))
+	if err != nil {
+		fmt.Printf("An error occured: failed to convert position_id to int [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
 
+	var position web.WorkingPosition
+	if err := c.BindJSON(&position); err != nil {
+		fmt.Printf("An error occured: failed to bind JSON [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	position.PositionID = position_id
+	h.service.UpdatePosition(position_id, position)
+
+	c.JSON(http.StatusOK, web.WorkingPosition{
+		PositionID: position_id,
+		Name:       position.Name,
+	})
 }
 
 func (h *Handler) deletePosition(c *gin.Context) {
+	position_id, err := strconv.Atoi(c.Param("position_id"))
+	if err != nil {
+		fmt.Printf("An error occured: failed to convert position_id to int [%s]", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorResponse{
+			Message: err.Error(),
+		})
+		return
+	}
 
+	h.service.DeletePosition(position_id)
+	c.String(http.StatusOK, "Position deleted")
 }
 
 func (h *Handler) createStaff(c *gin.Context) {
