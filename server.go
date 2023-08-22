489e283339381f79450b04895f6c87fa003e0cad
MRskyPG <mrogalsky8113@gmail.com>
2023-08-22 12:21:25 +0700

    Add constants

--- server.go
+++ server.go
@@ -10,12 +10,17 @@ type Server struct {
 	httpServer *http.Server
 }
 
+const (
+	defaultReadTimeout  = 10 * time.Second
+	defaultWriteTimeout = 10 * time.Second
+)
+
 func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
 		Handler:        handler,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    defaultReadTimeout,
+		WriteTimeout:   defaultWriteTimeout,
 		MaxHeaderBytes: 1 << 20, //1 MB
 	}
 	return s.httpServer.ListenAndServe()
