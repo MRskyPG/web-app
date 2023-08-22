92a4cabf959cf693c9120e258db8397a391da80a
MRskyPG <mrogalsky8113@gmail.com>
2023-08-22 12:24:01 +0700

    Add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/MRskyPG/web-app"
 	"github.com/MRskyPG/web-app/pkg/handler"
 	"github.com/MRskyPG/web-app/pkg/service"
+	"github.com/sirupsen/logrus"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,8 +17,21 @@ func main() {
 	handlers := handler.New(mapService)
 	var srv web.Server
 
-	if err := srv.Run("80", handlers.InitRoutes()); err != nil {
-		log.Fatalf("Error occured while running http server: %s", err.Error())
-	}
+	//Using graceful shutdown
+	go func() {
+		if err := srv.Run("80", handlers.InitRoutes()); err != nil {
+			log.Fatalf("Error occured while running http server: %s", err.Error())
+		}
+	}()
 
+	logrus.Print("Web-app Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Web-app Shutting Down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("Error was occured on server shutting down: %s", err.Error())
+	}
 }
